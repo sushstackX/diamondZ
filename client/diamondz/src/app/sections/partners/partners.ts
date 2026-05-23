fe52195392b7dc175e09835a39e229c3d36a9921
sushstackX <sushmasush0326@gmail.com>
2026-05-23 17:30:02 +0530

    partner file

--- client/diamondz/src/app/sections/partners/partners.ts
+++ client/diamondz/src/app/sections/partners/partners.ts
@@ -1,37 +1,74 @@
 import { Component } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
+import {
+  ShieldCheck,
+  Link2,
+  BadgeCheck,
+  FlaskConical,
+  Globe,
+  Landmark,
+  LucideAngularModule
+} from 'lucide-angular';
 
 interface Partner {
   name: string;
   logo: string;
+  country: string;
+  flag: string;
 }
 
 @Component({
   selector: 'app-partners',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    LucideAngularModule
+  ],
   templateUrl: './partners.html',
   styleUrls: ['./partners.css']
 })
+
 export class Partners {
 
+  readonly ShieldCheck = ShieldCheck;
+  readonly Link2 = Link2;
+  readonly BadgeCheck = BadgeCheck;
+  readonly FlaskConical = FlaskConical;
+  readonly Globe = Globe;
+  readonly Landmark = Landmark;
+
   partners: Partner[] = [
-{
-    name: 'Lubrizol',
-    logo: 'assets/images/lubrizol.png'
-  },
-  {
-    name: 'BASF',
-    logo: 'assets/images/lubrizol.png'
-  },
-  {
-    name: 'Covestro',
-    logo: 'assets/images/covestro.png'
-  },
-  {
-    name: 'Ashland',
-    logo: 'assets/images/ashland.png'
-  }
+
+    {
+      name: 'BASF',
+      logo: 'assets/images/basf.png',
+      country: 'Germany',
+      flag: 'assets/images/germany.png'
+    },
+
+    {
+      name: 'Lubrizol',
+      logo: 'assets/images/lubrizol.png',
+      country: 'USA',
+      flag: 'assets/images/usa.png'
+    },
+
+    {
+      name: 'Ashland',
+      logo: 'assets/images/ashland.png',
+      country: 'USA',
+      flag: 'assets/images/usa.png'
+    },
+
+    {
+      name: 'Covestro',
+      logo: 'assets/images/covestro.png',
+      country: 'Germany',
+      flag: 'assets/images/germany.png'
+    }
+
   ];
 
 }
